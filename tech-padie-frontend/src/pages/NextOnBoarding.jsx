84408d43e02725c30ce84249726715cbbbc3aff4
Dearie <eburujacintha@gmail.com>
2024-09-26 12:11:13 +0100

    fixed button error

--- tech-padie-frontend/src/pages/NextOnBoarding.jsx
+++ tech-padie-frontend/src/pages/NextOnBoarding.jsx
@@ -1,13 +1,24 @@
 import Button from "../components/Button";
+import Tracker from "../ui/Tracker";
+import ellipsebg from '../images/ellipsebg.png';
 
-function InitialOnBoarding() {
+function NextOnBoarding() {
   return (
-    <div className="bg-white p-24 sm:w-[90%] md:w-[70%] lg:w-[50%]">
+    <div className="bg-white p-24 sm:w-[90%] md:w-[70%] lg:w-[50%]"
+    style={{
+      backgroundImage: `url(${ellipsebg})`,
+      backgroundSize: 'cover', 
+      backgroundRepeat: 'no-repeat', 
+    }}
+    >
+      <Tracker currtrack={50} height={4} bgcolor="#F1F1F1">
+        <Tracker.Bar color="#227FA1" /> 
+      </Tracker>
       <div className="flex flex-col items-start">
         <h1 className="mb-4 text-4xl font-semibold text-blue-400">
         More Knowledge
         </h1>
-        <p className="text-md mb-4 text-black">
+        <p className="text-xl font-semibold mb-4 text-black">
         All in one place
         </p>
 
@@ -22,5 +33,4 @@ function InitialOnBoarding() {
   );
 }
 
-export default InitialOnBoarding;
-// import { useState } from 'react';
+export default NextOnBoarding;
